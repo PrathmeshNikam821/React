405c9633591d8324c0cf91337ba7313a2f295709
Pratham21 <prathamnikam21@gmail.com>
2025-06-14 13:13:14 +0530

    calculator v1 done

--- Projects/calculator-v1/src/components/ButtonContainer.jsx
+++ Projects/calculator-v1/src/components/ButtonContainer.jsx
@@ -1,15 +1,24 @@
 import styles from './ButtonContainer.module.css';
 
 const ButtonContainer = () =>{
+
+  const buttonNames = [ 'C', '1' , '2' ,'+' , '3','4','-', '5' ,'6' ,'*' , '7',
+      '8' , '/' , '=' , '9' , '0' , '.' ]
+
+
   return (
 
-     <div className={styles.buttonsContainer}>
 
-      <button  className={styles.button}>C</button>
-      <button className={styles.button}>1</button>
-      <button className={styles.button}>2</button>
-      <button className={styles.button}>+</button>
-      {/* <button>4</button> */}
+    <div className={styles.buttonsContainer}>
+      {buttonNames.map(buttonName =>{
+        return <button  className={styles.button}>{buttonName}</button>
+        
+      })}
+
+     
+
+      
+      
     </div>
   )
 }
