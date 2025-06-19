af82dd7c3956ee3579f6841df6e13e51bf9fcce4
Pratham21 <prathamnikam21@gmail.com>
2025-06-19 18:51:50 +0530

    learned useRef state

--- Projects/to-do-app - 3/src/components/AddTodo.jsx
+++ Projects/to-do-app - 3/src/components/AddTodo.jsx
@@ -1,40 +1,39 @@
-import { useState } from "react";
+import { useRef} from "react";
 import { BiSolidAddToQueue } from "react-icons/bi";
 
 
 function AddTodo({onNewItem}){
 
-  const [todoName , setTodoName] = useState("");
+ 
 
-  const [dueDate , setDueDate] = useState("") ; 
+  const todoNameElement = useRef();
 
-  const handleNameChange =(event) =>{
+  const dueDateElement = useRef();
 
-    setTodoName(event.target.value);
-  }
-
-  const handleDateChange = (event) =>{
-
-    setDueDate(event.target.value);
-   
-  }
+ 
 
   const handleAddButtonClicked = ( event) =>{
     
     event.preventDefault();
+    const todoName = todoNameElement.current.value; 
+    const dueDate = dueDateElement.current.value; 
+    todoNameElement.current.value="";
+    dueDateElement.current.value="";
+
 
     onNewItem(todoName, dueDate); 
-     setDueDate("");
-      setTodoName("");
+ 
   }
 
   return <div className="container text-center">
         <form action=""  onSubmit={handleAddButtonClicked} className=" row my-row">
           <div className="col-4">
-            <input type="text" placeholder="Enter to do " value={todoName} onChange={handleNameChange} />
+            <input type="text" ref={todoNameElement}
+            placeholder="Enter to do "   />
           </div>
           <div className="col-4">
-            <input type="date" value={dueDate} onChange={handleDateChange}/>
+            <input type="date" ref={dueDateElement}
+             />
           </div>
           <div className='col-2'>
             <button className="btn btn-success my-button"
