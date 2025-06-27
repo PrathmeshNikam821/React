799e7ce8c962d875d009270a2145351a22a582c6
Pratham21 <prathamnikam21@gmail.com>
2025-06-27 07:23:00 +0530

    useEffect and dummy APIs

---
+++ dummy API/src/components/PostList.jsx
@@ -0,0 +1,35 @@
+import { useContext, useEffect } from "react";
+import Post from "./Post";
+import { PostList as PostListData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMesaage";
+
+
+const PostList = () => {
+  const { postList , addInitialPosts } = useContext(PostListData);
+  useEffect(()=>{
+     fetch('https://dummyjson.com/posts')
+      .then(res => res.json())
+      .then(data =>{
+        addInitialPosts(data.posts )
+      })  
+  },[])
+
+
+  
+     
+
+
+
+
+  return (
+    <>
+
+      {postList.length === 0 && <WelcomeMessage  /> }
+      {postList.map((post) => (
+        <Post key={post.id} post={post}></Post>
+      ))}
+    </>
+  );
+};
+
+export default PostList;
